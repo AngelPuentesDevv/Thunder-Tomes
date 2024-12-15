f6104d029472ae2e11b1030ce6a8be74ca8447e8
Angel <apuentest@uniempresarial.edu.co>
2024-12-15 12:23:41 -0500

    first commit

---
+++ src/main/java/com/angelpuentesdevv/thundertomes/ThundertomesApplication.java
@@ -0,0 +1,13 @@
+package com.angelpuentesdevv.thundertomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThundertomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThundertomesApplication.class, args);
+	}
+
+}
